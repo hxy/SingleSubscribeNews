e24bd6ff87b8b5323adb12388be4b3fe5c68aa9f
huangyue <825503721@qq.com>
2015-09-15 21:49:22 +0800

    add action bar

--- src/com/hy/singlesubscribenews/activity/NewsDetialActivity.java
+++ src/com/hy/singlesubscribenews/activity/NewsDetialActivity.java
@@ -2,9 +2,12 @@ package com.hy.singlesubscribenews.activity;
 
 import com.hy.singlesubscribenews.R;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -13,6 +16,8 @@ public class NewsDetialActivity extends Activity{
 
 	private WebView webView;
 	private TextView textView;
+	private String newsUrl;
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -21,18 +26,42 @@ public class NewsDetialActivity extends Activity{
 		this.setContentView(R.layout.activity_detial);
 		
 		webView = (WebView)findViewById(R.id.webview);
-		textView = (TextView)findViewById(R.id.originalnews);
+		//textView = (TextView)findViewById(R.id.originalnews);
 		
-		
-		String url = getIntent().getStringExtra("newsUrl");
+		newsUrl = getIntent().getStringExtra("newsUrl");
 		String description = getIntent().getStringExtra("description");
-		this.setContentView(initWebView(url,description));
+        initWebView(description);
 	}
 	
-	private WebView initWebView(String url,String description){
-		WebView webView = new WebView(this);
+	private void initWebView(String description){
 		webView.getSettings().setDefaultTextEncodingName("UTF-8") ;
 		webView.loadData(description, "text/html;charset=UTF-8", null);
-		return webView;
 	}
+
+	private void goToOriginalNews(){
+		webView.loadUrl(newsUrl);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.detial, menu);  
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getOrder()) {
+		case 100:
+			goToOriginalNews();
+			break;
+
+		default:
+			break;
+		}
+		
+		return true;
+	}
+	
 }
